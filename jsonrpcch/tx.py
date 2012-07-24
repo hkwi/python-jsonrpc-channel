320e4264f87a709026893acee2992b92da6bf349
kawai <kawai@iij.ad.jp>
2012-07-24 22:35:58 +0900

    replace DeferredProxy with DeferredChannel
    The server registered at channel can return a deferred object
    as a response to a request.

--- jsonrpcch/tx.py
+++ jsonrpcch/tx.py
@@ -1,16 +1,26 @@
 # coding: UTF-8
 # This code is available from http://github.com/hkwi/python-jsonrpc-channel
+from twisted.internet.defer import Deferred
 from twisted.internet.protocol import Factory, Protocol, ProcessProtocol
 from twisted.web.resource import IResource
 from zope.interface import implements
 import jsonrpcch
+
+class DeferredChannel(jsonrpcch.Channel):
+	def serve_result_fixup(self, request, result):
+		if isinstance(result, Deferred):
+			def catch_result_fail(fail):
+				self.serve_error(request, fail.value)
+			result.addCallback(self.serve_result, catch_result_fail)
+		else:
+			result.serve_result(result)
 
 class JsonrpcProtocol(Protocol):
 	channel = None
 	
 	def _ensure_channel(self):
 		if self.channel is None:
-			self.channel = jsonrpcch.Channel()
+			self.channel = DeferredChannel()
 	
 	def connectionMade(self):
 		self._ensure_channel()
@@ -30,26 +40,6 @@ class JsonrpcProtocol(Protocol):
 		self._ensure_channel()
 		self.channel.register_server(server_instance)
 
-class _DeferredProxy(jsonrpcch.Proxy):
-	encoding = "UTF-8"
-	
-	def call(self, obj):
-		d = obj.get("result")
-		if isinstance(d, defer.Deferred):
-			d.addCallback(self._obj_to_callback)
-			d.addErrback(self.errback)
-		else:
-			self._obj_to_callback(obj)
-	
-	def _obj_to_stream(self, obj):
-		self.callback(json.dumps(obj).encode(self.encoding))
-	
-	def callback(self, binary):
-		warnings.warn("_DeferredProxy callback not registered")
-	
-	def errback(self, e):
-		warnings.warn("_DeferredProxy errback not registered")
-
 class JsonrpcResource:
 	implements(IResource)
 	
@@ -64,25 +54,15 @@ class JsonrpcResource:
 			request.finish()
 			return
 		
-		proxy = _DeferredProxy()
-		channel = jsonrpcch.Channel()
+		channel = DeferredChannel()
 		channel.register_server(self.server)
-		channel.proxy = proxy
-		proxy.encoding = channel.encoding
+		def body_sendout(binary):
+			request.setHeader("content-type", "application/json; charset=%s" % (proxy.encoding,))
+			request.setHeader("content-length", len(binary_or_error))
+			request.write(binary_or_error)
+			request.finish()
 		
-		def body_sendout(error=False):
-			def resp(binary_or_error):
-				if error:
-					request.setResponseCode(500)
-				else:
-					request.setHeader("content-type", "application/json; charset=%s" % (proxy.encoding,))
-					request.setHeader("content-length", len(binary_or_error))
-					request.write(binary_or_error)
-				request.finish()
-		
-		proxy.callback = channel.sendout = body_sendout()
-		proxy.errback = body_sendout(True)
-		
+		channel.sendout = body_sendout
 		channel.feed(request.content.read())
 		return NOT_DONE_YET
 	
